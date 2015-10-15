c42abe7e509514877ba202d3ab1357f2ed05dbb2
Outpox <guillaumeoutpox@gmail.com>
2015-10-15 10:17:23 +0200

    Reworked mailList

--- background.js
+++ background.js
@@ -1,6 +1,4 @@
 var domain = "yopmail.com";
-var mailList = [];
-mailList.counter = 10;
 
 var Mail = function () {
 	this.name = randomMail();
@@ -9,9 +7,16 @@ var Mail = function () {
 	this.lastUsed = null;
 	this.id = mailList.counter++;
 	this.usageCount = 0;
-	mailList.push(this);
+	mailList.addMail(this);
 	mailList.saveAndSync();
 };
+
+var MailList = function(counter) {
+	this.counter = (typeof counter === 'undefined') ? 0 : counter;
+	this.content = [];
+}
+
+var mailList = new MailList(10);
 
 function getMail(id) {
 	for (var i = 0; i < mailList.length; i++) {
@@ -21,30 +26,32 @@ function getMail(id) {
 	return null;
 }
 
-mailList.saveAndSync = function() {
+MailList.prototype.saveAndSync = function() {
 	// chrome.storage.sync.set({"yh_mailList": mailList});
-	chrome.storage.sync.set({"yh_mailList": mailList});
+	chrome.storage.sync.set({"yh_mailList": this});
 };
-mailList.updateValues = function (newVal) {
-	mailList.length = 0;
-	for (var i = 0; i < newVal.length; i++) {
-		mailList[i] = newVal[i];
-	};
+MailList.prototype.updateValues = function (newArray) {
+	this.content = newArray;
 };
-mailList.load = function () {
+MailList.prototype.load = function () {
 	chrome.storage.sync.get("yh_mailList", function(data) {
+		console.log(data);
 		if (data.yh_mailList !== undefined) {
-			mailList = data.yh_mailList;
+			this.content = data.yh_mailList.content;
+			this.counter = data.yh_mailList.counter;
 		}
 	})
 };
-
-function deleteMail(id) {
-	for (var i = 0; i < mailList.length; i++) {
-		if (mailList[i].id === id)
-			mailList.splice(i, 1);
+MailList.prototype.deleteMail = function(id) {
+	for (var i = 0; i < this.content.length; i++) {
+		if (this.content[i].id === id)
+			this.content.splice(i, 1);
 	};
-}
+	this.saveAndSync();
+};
+MailList.prototype.addMail = function(mail) {
+	this.content.push(mail);
+};
 
 function randomMail() {
 	return (Math.random()*1e32).toString(36);
@@ -62,11 +69,11 @@ chrome.runtime.onMessage.addListener(
 				mailList.saveAndSync();
 				break;
 			case "getMailList":
-				sendResponse({message: "OK", mailList: mailList});
+				sendResponse({message: "OK", mailList: mailList.content});
 				break;
 			case "deleteMail":
-				deleteMail(request.value);
-				sendResponse({message: "OK", mailList: mailList});
+				mailList.deleteMail(request.value);
+				sendResponse({message: "OK", mailList: mailList.content});
 				break;
 		}
 	}
